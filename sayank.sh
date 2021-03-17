db75a5bf58b1d7f64e61e62fb92a85b4a98172ba
MR M3R4Z <77550679+Ardi12345X@users.noreply.github.com>
2021-03-18 01:01:15 +0700

    Add files via upload

---
+++ sayank.sh
@@ -0,0 +1,46 @@
+sleep 2
+echo -e "\e[94mhalo bro :)\e[0m";
+sleep 3
+echo -e "\e[92mhecked by.M3R4Z\e[0m";
+sleep 5
+echo -e "\e[91mtutorial nembak cewek!\e[0m";
+sleep 4
+echo -e "\e[91mtutorial yang sangat mudah dan pasti berhasi, tapi tidak 100%berhasl\e[0m";
+sleep 5
+echo -e "\e[92mbahan-bahan yang dibutuhkan \e[0m";
+sleep 5
+echo -e "\e[92m1 batu\e[0m";
+sleep 3
+echo -e "\e[93m2 uang ,kalau kalian gk punya uang juga gak papa ,asalkan si cewek nya nanti gak nangis,kalau si cewek nya nangis itu tanggung jawab kalian
+\e[0m";
+sleep 5
+echo -e "\e[94m3 bunga,terserang mau bunga apa aja yang penting bunga nya bagus dan wangi\e[0m";
+sleep 3
+echo -e "\e[92m4 kondom -\#canda bng :)\e[0m";
+sleep 3
+echo -e "\e[94mok itu bahan-bahan yang dibutuhkan 
+jika sudah siap, kita lanjut ke cara kerja nya\e[0m";
+sleep 4
+echo -e "\e[94m1 - pastikan kalian dekat dengan target kalian,tapi jangan terlalu dekat,kira-kira kurang lebih 10 meter\e[0m";
+sleep 5
+echo -e "\e[92m2 pastikan orangtua kalian gak ada,jika ortu kalian ada ,tutor ini gak bakal 100%  berhasi \e[0m";
+sleep 3
+echo -e "\e[94m3 ambil batu terus lempar ke kaki atau badan si cewek jangan ke susu nya :) 
+ ingat lempar nya jangan ke kepala si cewek ,\e[0m";
+sleep 5
+echo -e "\e[92m5 jika si cewek mengejar-ngejar kamu berarti tutor ini berhasi.
+jika si cewek hanya marah berarti tutor ini gagal.
+apabila si cewek nangis mending langsung kabur\e[0m";
+sleep 6
+echo -e "\e[95mtapi kalau tempat nya sepi\"mending cewek nya di peluk jangan kabur, ingat cuma di peluk jangan di hamili ,\e[0m";
+sleep 4
+echo -e "\e[91msekian tutorial dari saya :)\e[0m";
+sleep 6
+echo -e "\e[94mdosa tanggung sendiri!\e[0m";
+sleep 5
+echo -e "\e[92mhecked by.M3R4Z\e[0m";
+sleep 3
+echo -e "\e[37mketik exit untuk keluar \e[0m";
+read -p '$>' userinput1;
+#
+sleep 5
